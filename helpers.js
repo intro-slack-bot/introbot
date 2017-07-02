a33abf0ac214df9223e6d377b30f238bbb8d5d62
System <none>
2017-07-02 17:43:23 +0000

    Checkpoint

---
+++ helpers.js
@@ -0,0 +1,12 @@
+const request = require('request-promise');
+
+const slack = (api, data={}, method='POST') => {
+  return request({
+        method : 'POST',
+        uri: `https://slack.com/api/${api}`,
+        form: data,
+        headers: {
+           'content-type': 'application/x-www-form-urlencoded' 
+        }
+      });
+}

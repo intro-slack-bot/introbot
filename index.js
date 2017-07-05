116a585f43c5ac77423b60f6e70bd825b49dbf69
System <none>
2017-07-05 05:03:51 +0000

    Checkpoint

--- index.js
+++ index.js
@@ -81,8 +81,11 @@ rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {//@why we need to
       let re = /<@\w*>/i; //to get @username string from the message - this will have the user-id , not user-name. 
       if(msg.match(re)){
         let user = msg.match(re);
+        
+        /*
         let username = user.name;
         let toBeThankedUserId = user[0].substring(2, user[0].length - 1);
+        */
         let team = rtm.dataStore.getTeamById(rtm.activeTeamId);
         let teamname = team.name;
         //need to
